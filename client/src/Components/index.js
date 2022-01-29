26b20fe6ac76ec14cafc8e49204e01561ab3b620
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-29 10:59:36 +0100

    chore: exported all components

---
+++ client/src/Components/index.js
@@ -0,0 +1,6 @@
+export {default as Loader} from './Loader.components'
+export { default as Footer } from './Footer.components'
+export { default as Navbar } from './Navbar.component.jsx'
+export { default as Transactions } from './Transactions.components'
+export { default as Welcome } from './Welcome.components'
+export { default as Services } from './Services.components'
