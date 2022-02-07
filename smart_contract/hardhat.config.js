04f6597d5daa621e993419761071815ccc32e1e0
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-02-07 11:57:01 +0100

    Added module export for network and account

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
+require('@nomiclabs/hardhat-waffle')
+
+module.export = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/90A-t4dNYCpbHK29HkfqTn3ek6OBYb8g',
+      account: ['61ff41ce83111d9ed9d44bdc79ad1d62392373be5d881d632a9e05a1140ea1c1']
+    }
   }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
-};
+}
